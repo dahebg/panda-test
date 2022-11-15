248eefeba579dbcc603a23bd85c76fb711c22c8d
godv <2461212309@qq.com>
2022-11-15 22:12:50 +0800

    App.vue 改为 rename.vue 了

---
+++ src/rename.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>添加的改动3</h1>
+    <h2>我又改变了</h2>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
